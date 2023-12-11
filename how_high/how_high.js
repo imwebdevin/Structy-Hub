19797086685d9deefd529ebec8a606ed331e9230
imwebdevin <126222174+imwebdevin@users.noreply.github.com>
2023-12-11 12:16:26 -0500

    Solved 'how high' (.js)

---
+++ how_high/how_high.js
@@ -0,0 +1,24 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+​
+const howHigh = (node) => {
+  // todo
+  if (node === null) return -1;
+  let leftHeight = howHigh(node.left);
+  let rightHeight = howHigh(node.right);
+  if (node.left || node.right) return 1 + Math.max(leftHeight, rightHeight);
+  return 0
+  
+  
+  
+};
+​
+module.exports = {
+  howHigh,
+};
+​
