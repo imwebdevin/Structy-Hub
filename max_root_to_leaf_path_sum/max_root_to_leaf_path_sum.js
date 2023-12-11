def58742c4041efd713cd502cfdfae10bc9be950
imwebdevin <126222174+imwebdevin@users.noreply.github.com>
2023-12-10 19:57:06 -0500

    Solved 'max root to leaf path sum' (.js)

--- max_root_to_leaf_path_sum/max_root_to_leaf_path_sum.js
+++ max_root_to_leaf_path_sum/max_root_to_leaf_path_sum.js
@@ -9,8 +9,8 @@
 const maxPathSum = (root) => {
   // todo
   if (root === null) return -Infinity
-  if (!root.left && !root.right) return root.val
-  return  Math.max(root.val + maxPathSum(root.left), root.val + maxPathSum(root.right)); 
+  if (!root.left && !root.right) return root.val;
+  return root.val + Math.max(maxPathSum(root.left), maxPathSum(root.right)); 
 ​
 };
 ​
