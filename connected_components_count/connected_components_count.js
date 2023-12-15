87e444918e7a0c4272ebc96e2f4d336c3110b270
imwebdevin <126222174+imwebdevin@users.noreply.github.com>
2023-12-15 15:47:22 -0500

    Solved 'connected components count' (.js)

--- connected_components_count/connected_components_count.js
+++ connected_components_count/connected_components_count.js
@@ -5,11 +5,7 @@ const connectedComponentsCount = (graph) => {
   const stack = [];
   
   for (const key in graph) {
-    // keys.push(graph[key])
-    console.log(key, "key")
     if (!visited.has(key)) {
-      console.log(`haven't visited ${key} yet!. adding ${key} to visited`)
-      console.log(visited)
       stack.push(key);
       count++
       while (stack.length > 0) {
@@ -17,21 +13,14 @@ const connectedComponentsCount = (graph) => {
         if (!visited.has(curr))
           visited.add(curr);
           for (const neighbor of graph[curr]) {
-            if (!visited.has(neighbor.toString())) {
-              visited.add(neighbor.toString)
-              stack.push(neighbor.toString())
+            const neighborAsString = neighbor.toString()
+            if (!visited.has(neighborAsString) {
+              visited.add(neighborAsString)
+              stack.push(neighborAsString)
             }
-          }
-        
+          } 
       }
     }
-    // for (const neighbor of graph[key]) {
-    //   console.log(neighbor)
-    //   if (!visited.has(neighbor.toString())) {
-    //     // console.log(neighbor, key)
-    //     visited.add(neighbor.toString())
-    //   }
-    // }
   }
   return count;
 };
